c7bd6dd17fc9cb746994e0a12ee78cad8a405e06
Rod Johnson <johnsonroda@gmail.com>
2018-02-23 18:31:54 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Param1Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Param1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Param1Application.class, args);
+	}
+}
